6d87edd1b7da3c53d466113f176d610fdb146ccb
Oskar Lindblad <lindbladoskar@gmail.com>
2019-11-10 13:56:43 +0100

    getting Ready for lightbox-try

--- script.js
+++ script.js
@@ -7,10 +7,27 @@ const secretCode = 'potato';
      pressed.push(e.key);
      pressed.splice(-secretCode.length - 1, secretCode.length - secretCode.length);
     if(pressed.join('').includes(secretCode)){
-        console.log('njds');
         document.body.style.backgroundImage = "url('images/potato.png')";
         document.body.style.backgroundSize = "3rem";
     
     }
  })
 
+/**
+ * Takes the user to the top of the site
+ */
+
+ window.addEventListener('scroll', () => {
+     const scrolledDistance = window.scrollY;
+     console.log(scrolledDistance);
+    if(scrolledDistance > 150){
+    document.getElementById("upButton").style.display = "block";
+    } else if (scrolledDistance <= 150){
+        document.getElementById("upButton").style.display = "none";
+        }
+ });
+
+ function goToTheTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
