95576f6f84a8297d6a992429c59423e3f1b59b2a
Oskar Lindblad <lindbladoskar@gmail.com>
2019-11-13 19:54:35 +0100

    Freshing up js

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ const secretCode = 'potatis';
     if(pressed.join('').includes(secretCode)){
         document.body.style.backgroundImage = "url('images/potato.png')";
         document.body.style.backgroundSize = "3rem";
-        document.getElementsByClassName("boxKuritorn")[0].style.backgroundImage = "url('images/potatto_mode.gif')";
-        document.getElementsByClassName("boxAlingsas")[0].style.backgroundImage = "url('images/potatto_mode.gif')";
-        document.getElementsByClassName("boxRimlikt")[0].style.backgroundImage = "url('images/potatto_mode.gif')";
-        document.getElementsByClassName("boxVilsekomna")[0].style.backgroundImage = "url('images/potatto_mode.gif')";
-        document.getElementsByClassName("boxPotatamon")[0].style.backgroundImage = "url('images/potatto_mode.gif')";
-        document.getElementsByClassName("boxLjudbok")[0].style.backgroundImage = "url('images/potatto_mode.gif')";
-        document.getElementsByClassName("boxJobbbackground")[0].style.backgroundImage = "url('images/potatis_still.png')";
-        document.getElementsByClassName("boxUtbildningbackground")[0].style.backgroundImage = "url('images/potatis_still.png')";
-        document.getElementsByClassName("boxKontaktbackground")[0].style.backgroundImage = "url('images/potatis_still.png')";
+        document.querySelector(".boxKuritorn").style.backgroundImage = "url('images/potatto_mode.gif')";
+        document.querySelector(".boxAlingsas").style.backgroundImage = "url('images/potatto_mode.gif')";
+        document.querySelector(".boxRimlikt").style.backgroundImage = "url('images/potatto_mode.gif')";
+        document.querySelector(".boxVilsekomna").style.backgroundImage = "url('images/potatto_mode.gif')";
+        document.querySelector(".boxPotatamon").style.backgroundImage = "url('images/potatto_mode.gif')";
+        document.querySelector(".boxLjudbok").style.backgroundImage = "url('images/potatto_mode.gif')";
+        document.querySelector(".boxJobbbackground").style.backgroundImage = "url('images/potatis_still.png')";
+        document.querySelector(".boxUtbildningbackground").style.backgroundImage = "url('images/potatis_still.png')";
+        document.querySelector(".boxKontaktbackground").style.backgroundImage = "url('images/potatis_still.png')";
     }
  })
 
@@ -140,27 +140,101 @@ const secretCode = 'potatis';
 
 
 
-  /**Mobile Meny resize */
-  document.querySelector('.boxKontakt').addEventListener("click", reziseBoxKontakt);
+  /**
+  * @param {string} boxKontakt - Control boxKontakt div
+  * @param {string} boxKontakt - Control the hidden content in boxKontakt div
+  */
+  const boxKontakt = document.querySelector('.boxKontakt');
+  const boxKontaktHidden = document.querySelector('.boxKontakt .hideInMobile');
 
-  function reziseBoxKontakt(){
-    if (window.innerWidth <= 768) {
-      document.querySelector('.boxKontakt').style.height = "14rem";
-      document.querySelector('.boxKontakt .hideInMobile').style.display = "block";
+  boxKontakt.addEventListener("click", function() {
+    if (window.innerWidth <= 767) {
+      boxKontakt.style.height = "14rem";
+      boxKontaktHidden.style.display = "block";
     }
+  });
 
-  }
+  /**
+  * @param {string} boxOmMig - Control boxOmMig div
+  * @param {string} boxOmMigHidden - Control the hidden content in boxOmMig div
+  */
+ const boxOmMig = document.querySelector('.boxOmMig');
+ const boxOmMigHidden = document.querySelector('.boxOmMig .hideInMobile');
 
-  document.querySelector('.boxOmMig').addEventListener("click", reziseboxOmMig);
-
-  function reziseboxOmMig(){
-    if (window.innerWidth <= 768) {
-      document.querySelector('.boxOmMig').style.height = "42.5rem";
-      document.querySelector('.boxOmMig .hideInMobile').style.display = "block";
+  boxOmMig.addEventListener("click", function() {
+    if (window.innerWidth <= 767) {
+      boxOmMig.style.height = "42.5rem";
+      boxOmMigHidden.style.display = "block";
     }
+  });
 
-  }
+  /**
+  * @param {string} boxUtbildning - Control boxUtbildning div
+  * @param {string} boxUtbildningHidden - Control the hidden content in boxUtbildning div
+  */
+ const boxUtbildning = document.querySelector('.boxUtbildning');
+ const boxUtbildningHidden = document.querySelector('.boxUtbildning .hideInMobile');
 
+ boxUtbildning.addEventListener("click", function() {
+    if (window.innerWidth <= 767) {
+      boxUtbildning.style.height = "17rem";
+      boxUtbildningHidden.style.display = "block";
+    }
+  });
+
+
+  /**
+  * @param {string} boxJobb - Control boxJobb div
+  * @param {string} boxJobbHidden - Control the hidden content in boxJobb div
+  */
+ const boxJobb = document.querySelector('.boxJobb');
+ const boxJobbHidden = document.querySelector('.boxJobb .hideInMobile');
+
+ boxJobb.addEventListener("click", function() {
+    if (window.innerWidth <= 767) {
+      boxJobb.style.height = "27rem";
+      boxJobbHidden.style.display = "block";
+    }
+  });
+
+
+
+  /**
+   * 
+   * ELSE IF SOM ELSE med samma content
+   * 
+   * 
+   */
   window.addEventListener('resize', function() {
-    console.log(window.innerWidth)
-  })
+    if(window.innerWidth >= 768 && window.innerWidth <= 991) {
+      boxKontakt.style.height = "17rem;";
+      dboxKontaktHidden.style.display = "block";
+      boxOmMig.style.height = "16rem";
+      boxOmMigHidden.style.display = "block";
+      boxUtbildning.style.height = "14rem";
+      boxUtbildningHidden.style.display = "block";
+      boxJobb.style.height = "29rem";
+      boxJobbHidden.style.display = "block";
+
+    }else if(window.innerWidth >= 992 && window.innerWidth <= 1199){
+      boxKontakt.style.height = "17rem;";
+      boxOmMig.style.height = "15rem";
+      boxUtbildning.style.height = "17rem";
+      boxJobb.style.height = "17rem";
+    }else if(window.innerWidth >= 1200){
+      boxKontakt.style.height = "15rem;";
+      boxOmMig.style.height = "16rem";
+      boxUtbildning.style.height = "15rem";
+      boxJobb.style.height = "16rem";
+    } else{
+      boxKontakt.style.height = "1.5rem";
+      dboxKontaktHidden.style.display = "none";
+      boxOmMig.style.height = "1.5rem";
+      boxOmMigHidden.style.display = "none";
+      boxUtbildning.style.height = "1.5rem";
+      boxUtbildningHidden.style.display = "none";
+      boxJobb.style.height = "1.5rem";
+      boxJobbHidden.style.display = "none";
+  }
+  }
+  )
